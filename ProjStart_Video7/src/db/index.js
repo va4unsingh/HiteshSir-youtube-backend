5787015e3adb77ebec2e9d50cdd3bd50089ae946
Varun Singh <varunsingh7004@gmail.com>
2025-07-18 22:01:36 +0530

    video 8 done

--- ProjStart_Video7/src/db/index.js
+++ ProjStart_Video7/src/db/index.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
     );
-    console.log(connectionInstance);
+    // console.log("vader", connectionInstance);
   } catch (error) {
     console.error("MongoDB connection error ", error);
     process.exit(1);
