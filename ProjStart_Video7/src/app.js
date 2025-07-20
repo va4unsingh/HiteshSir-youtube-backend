4913c26b24e59059a0f6063afc5f741d69e45bbf
Varun Singh <varunsingh7004@gmail.com>
2025-07-20 12:33:53 +0530

    Video 9 Done

--- ProjStart_Video7/src/app.js
+++ ProjStart_Video7/src/app.js
@@ -9,7 +9,7 @@ app.use(
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   }),
-);
+); 
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
