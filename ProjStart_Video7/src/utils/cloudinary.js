b5c918851e969065c680f7ec6d6d99d59b461295
Varun Singh <varunsingh7004@gmail.com>
2025-07-23 20:27:19 +0530

    env done

--- ProjStart_Video7/src/utils/cloudinary.js
+++ ProjStart_Video7/src/utils/cloudinary.js
@@ -6,3 +6,30 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // File has been uploaded successfully
+    console.log("File is uploaded on cloudinary", response.url);
+    // Return the response to user
+    return response;
+  } catch (error) {
+    // Remove the locally saved temporary file as the upload operation got failed
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+
+    console.error("Cloudinary upload failed:", error);
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
