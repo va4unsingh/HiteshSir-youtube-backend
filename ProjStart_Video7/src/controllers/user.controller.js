991cc3f4e629ca85b944c0ebb09418a64533fadf
Varun Singh <varunsingh7004@gmail.com>
2025-07-24 18:54:29 +0530

    video 14 mid way

--- ProjStart_Video7/src/controllers/user.controller.js
+++ ProjStart_Video7/src/controllers/user.controller.js
@@ -1,9 +1,31 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Vader",
-  });
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    throw new ApiError(400, "Invalid email format");
+  }
 });
 
 export { registerUser };
