05c03bf66e1a5c9f69d62505b3c850136ffd693b
Varun Singh <varunsingh7004@gmail.com>
2025-07-28 19:37:02 +0530

    updateAccountDetails user controller done

--- ProjStart_Video7/src/controllers/user.controller.js
+++ ProjStart_Video7/src/controllers/user.controller.js
@@ -270,7 +270,28 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(200, req.user, "Current user fetched Successfully");
 });
 
-const updateAccountDetails = asyncHandler(async (req, res) => {});
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { new: true },
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiRes(200, user, "Account Details upadted Successfully"));
+});
 
 export {
   registerUser,
