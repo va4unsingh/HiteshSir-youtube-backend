eb135d6fed199f581bd5c0bb77f996910346d419
Varun Singh <varunsingh7004@gmail.com>
2025-07-30 17:41:56 +0530

    Add comment, like, playlist, and tweet models with schemas

--- ProjStart_Video7/src/models/comment.models.js
+++ ProjStart_Video7/src/models/comment.models.js
@@ -1,11 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const commentSchema = new Schema({
-    content:{
-        type:
-    }
-});
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 commentSchema.plugin(mongooseAggregatePaginate);
 
